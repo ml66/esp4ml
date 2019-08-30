e2a0b86550232608a4a201730bf317143f825b42
ml66 team <ml66team@gmail.com>
2019-08-30 16:53:32 -0400

    Add DATAFLOW to pipeline load/compute/store + ping/pong

--- accelerators/vivado_hls/adder/syn/directives.tcl
+++ accelerators/vivado_hls/adder/syn/directives.tcl
@@ -12,3 +12,4 @@ set_directive_data_pack "adder_dma${dma}" store_ctrl
 set_directive_interface -mode ap_fifo -depth 64 "adder_dma${dma}" in1
 set_directive_interface -mode ap_fifo -depth 32 "adder_dma${dma}" out
 set_directive_loop_tripcount -min 1 -max 1 -avg 1 "adder_dma${dma}/go"
+set_directive_dataflow "adder_dma${dma}/go"
